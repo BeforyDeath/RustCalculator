3927f4a238f84ea5a4f5fe2c195330449e19e019
beforydeath <beforydeath@gmail.com>
2015-03-06 02:19:19 +0300

    add event test

--- ts/Entity.js
+++ ts/Entity.js
@@ -2,6 +2,13 @@
  * Created by beforydeath on 05.03.15.
  */
 /// <reference path="Rust.ts"/>
+/// <reference path="EventMixin.ts"/>
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
 /*
  Entity
  -listeners[]
@@ -12,19 +19,12 @@
  -onRemove(id, count=1)
  +draw()
  -drawUpdate(id)
-/**
- * Created by beforydeath on 05.03.15.
  */
-/// <reference path="Rust.ts"/>
-/*
- Entity
- -listeners[]
- +load(url?)
- +loadData(json)
- +subscribe(class, event?)
- -onAdd(id, count=1)
- -onRemove(id, count=1)
- +draw()
- -drawUpdate(id)
- 
+var Entity = (function (_super) {
+    __extends(Entity, _super);
+    function Entity() {
+        _super.apply(this, arguments);
+    }
+    return Entity;
+})(EventMixin);
 //# sourceMappingURL=Entity.js.map

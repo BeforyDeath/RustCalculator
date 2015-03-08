27f4bd36acccfb40c3e2c27c7d742c9b940e4080
beforydeath <beforydeath@gmail.com>
2015-03-08 23:49:08 +0300

    init inventory

--- ts/Inventory.js
+++ ts/Inventory.js
@@ -2,36 +2,71 @@
  * Created by beforydeath on 05.03.15.
  */
 /// <reference path="Rust.ts"/>
-/*
- Inventory
- -listeners[]
- +load(url?)
- +loadData(json)
- +save(url?)
- +getData()
- -addEntity(id, count)
- -removeEntity(id, count, slot?)
- +subscribe(class, event?)
- +sort(desc?)
- +clear()
- //-onAdd(id, count)
- //-onRemove(id, count)
- -onClick(id, countEntityInSlot)
- -onClear()
- +draw()
- -drawUpdate(id)
- */
 var __extends = this.__extends || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
     __.prototype = b.prototype;
     d.prototype = new __();
 };
+/*
+draw
+load
+sort
+
+draw kit
+
+
+
+ */
 var Inventory = (function (_super) {
     __extends(Inventory, _super);
     function Inventory() {
         _super.apply(this, arguments);
+        this.slot = 36;
+        this.size = 65;
+        this.data = {};
     }
+    Inventory.prototype.load = function (url) {
+        var self = this;
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            async: false,
+            success: function (data) {
+                self.loadData(data);
+            }
+        });
+    };
+    Inventory.prototype.loadData = function (json) {
+        var _data = {};
+        this.slot = json.inventory.countSlot;
+        $.each(json.inventory.slot, function (key, value) {
+            _data[key] = {
+                id: value.entity.replace(/ /g, '_'),
+                name: value.entity,
+                count: value.count
+            };
+        });
+        this.data = _data;
+    };
+    Inventory.prototype.getData = function () {
+        return this.data;
+    };
+    Inventory.prototype.setData = function (data) {
+        this.data = data;
+    };
+    Inventory.prototype.draw = function (selector) {
+        var self = this;
+        self.selector = selector;
+        for (var i = 0; i < this.slot; i++) {
+            var entity = $('<button/>', {
+                id: 'slot' + i,
+                class: 'ei_' + this.size
+            });
+            //$(entity).on('click', self.onClick);
+            $(selector).append(entity);
+        }
+    };
     return Inventory;
 })(EventMixin);
 //# sourceMappingURL=Inventory.js.map
--- ts/Inventory.ts
+++ ts/Inventory.ts
@@ -2,26 +2,67 @@
  * Created by beforydeath on 05.03.15.
  */
 /// <reference path="Rust.ts"/>
+
 /*
- Inventory
- -listeners[]
- +load(url?)
- +loadData(json)
- +save(url?)
- +getData()
- -addEntity(id, count)
- -removeEntity(id, count, slot?)
- +subscribe(class, event?)
- +sort(desc?)
- +clear()
- //-onAdd(id, count)
- //-onRemove(id, count)
- -onClick(id, countEntityInSlot)
- -onClear()
- +draw()
- -drawUpdate(id)
+draw
+load
+sort
+
+draw kit
+
+
+
  */
 
 class Inventory extends EventMixin {
+    public slot:number = 36;
+    public size:number = 65;
+    private data = {};
+    private selector:string;
 
+    load(url:string) {
+        var self = this;
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            async: false,
+            success: function (data) {
+                self.loadData(data);
+            }
+        });
+    }
+
+    private loadData(json) {
+        var _data = {};
+        this.slot = json.inventory.countSlot;
+        $.each(json.inventory.slot, function (key, value) {
+            _data[key] = {
+                id: value.entity.replace(/ /g, '_'),
+                name: value.entity,
+                count: value.count
+            };
+        });
+        this.data = _data;
+    }
+
+    getData() {
+        return this.data;
+    }
+
+    setData(data) {
+        this.data = data;
+    }
+
+    draw(selector:string) {
+        var self = this;
+        self.selector = selector;
+        for (var i = 0; i < this.slot; i++) {
+            var entity = $('<button/>', {
+                id: 'slot' + i,
+                class: 'ei_'+this.size
+            });
+            //$(entity).on('click', self.onClick);
+            $(selector).append(entity);
+        }
+    }
 }

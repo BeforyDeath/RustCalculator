fdbc3b2c364984ac6589a6e33521677c602c030e
beforydeath <beforydeath@gmail.com>
2015-03-09 23:17:23 +0300

    add kit class

---
+++ ts/Kit.js
@@ -0,0 +1,15 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+/// <reference path="Rust.ts"/>
+var Kit = (function (_super) {
+    __extends(Kit, _super);
+    function Kit() {
+        _super.apply(this, arguments);
+    }
+    return Kit;
+})(Rust);
+//# sourceMappingURL=Kit.js.map
